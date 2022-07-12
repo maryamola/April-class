b6438ccb640ee7566ca4b527a609cb5bfe2cac1a
root <root@ip-172-31-27-104.ec2.internal>
2022-07-12 15:09:24 +0000

    added new sh

---
+++ hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ls
+echo "hello world"
+echo "cleanin the directors"
+sleep ss
+clear
+echo "job completed"
---
+++ helo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ls
+echo "hello world"
+echo "cleaning the director"
+sleep ss
+pwd
+clear
+echo "job completed"
